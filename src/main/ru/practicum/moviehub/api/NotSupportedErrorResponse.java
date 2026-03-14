54bdfb425fc1f28ace370be3322aae86f118dafb
Alexandra Tropina <alexandratropina@mail.ru>
2026-03-14 15:56:49 +0700

    Исправлена ошибка в NotSupportedErrorResponse

--- src/main/ru/practicum/moviehub/api/NotSupportedErrorResponse.java
+++ src/main/ru/practicum/moviehub/api/NotSupportedErrorResponse.java
@@ -3,7 +3,7 @@ package ru.practicum.moviehub.api;
 import java.util.Arrays;
 
 public class NotSupportedErrorResponse extends ErrorResponse {
-    public final static String ERROR = "Операция не поддерживается";
+    public static final String ERROR = "Операция не поддерживается";
 
     public NotSupportedErrorResponse(String[] details) {
         super(details);
